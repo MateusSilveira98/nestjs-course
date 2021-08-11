c121d895e7786d8ae01adc1626e4a61301401c1a
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-08-10 21:46:13 -0300

    nestJS course part2

--- shared/course.ts
+++ shared/course.ts
@@ -1,14 +1,35 @@
+import { IsBoolean, IsInt, IsMongoId, IsString } from "class-validator";
 
-export interface Course {
+export class Course {
+  @IsString()
+  @IsMongoId()
   _id: string;
+
+  @IsInt({message: 'seqNo must be numeric'})
   seqNo:number;
+
+  @IsString({always: false})
   url:string;
+
+  @IsString()
   iconUrl: string;
+
+  @IsString()
   courseListIcon: string;
+
+  @IsString()
   description: string;
+
+  @IsString()
   longDescription?: string;
+
+  @IsString()
   category: string;
+
+  @IsInt({message: 'seqNo must be numeric'})
   lessonsCount: number;
+
+  @IsBoolean()
   promo: boolean;
 }
 
